5f7742a70edc3ebd57fc404ba99dd421e6c2ce73
sujiehao <sujiehao@work.com>
2019-12-24 20:40:22 +0800

    a

--- src/engine/config/Config.ts
+++ src/engine/config/Config.ts
@@ -1,11 +1,11 @@
 export class Config {
 
     // public static Pic_Url:string = "http://localhost:9666/"
-    public static Pic_Url:string = "http://sujiehao.club:9666"
+    public static Pic_Url:string = "https://sujiehao.club:9666"
     // public static Compress_Url:string = "http://localhost:9666/upload/"
-    public static Compress_Url:string = "http://sujiehao.club:9666/upload/"
+    public static Compress_Url:string = "https://sujiehao.club:9666/upload/"
     // public static Convert_Url:string = "http://localhost:9666/convertwebp/"
-    public static Convert_Url:string = "http://sujiehao.club:9666/convertwebp/"
+    public static Convert_Url:string = "https://sujiehao.club:9666/convertwebp/"
 
 
 }
