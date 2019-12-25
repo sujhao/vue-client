25b304bf4eaf9394b81268f123664f39139d96d8
sujiehao <sujiehao@work.com>
2019-12-25 14:23:37 +0800

    a

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import 'element-ui/lib/theme-chalk/index.css';
 import { Logger } from './engine/utils/Logger';
 
 Vue.config.productionTip = false
+// 务必在加载 Vue 之后，立即同步设置以下内容
+Vue.config.devtools = false //生产版本默认为 false
 Vue.use(ElementUI);
 new Vue({
   router,
